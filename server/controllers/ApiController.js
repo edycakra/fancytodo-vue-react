c670c0e563aa7e75bf70f508214ba13d43b28e44
edycakra <edycakrasinaga@gmail.com>
2020-02-29 18:38:04 +0700

    3rd api is added

--- server/controllers/ApiController.js
+++ server/controllers/ApiController.js
@@ -2,15 +2,17 @@ const axios = require('axios')
 
 class ApiController {
     static getApi (req, res, next) {
+        console.log('>>>', process.env.API_KEY)
         axios({
             url: `https://api.airvisual.com/v2/nearest_city?key=${process.env.API_KEY}`,
             method: `GET`,
             responseType: `json`
         })
-        .then(data => {
-            res.status(200).json({data})
+        .then(cities => {
+            res.status(200).json(cities.data)
         })
         .catch(err => {
+            console.log(err)
             next(err)
         })
     }
